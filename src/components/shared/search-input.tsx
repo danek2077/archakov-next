16ccf175504a683202e2b86c6d757a77febe1b26
danek2077 <pprezident337@gmail.com>
2024-12-16 16:43:55 +0100

    dev

--- src/components/shared/search-input.tsx
+++ src/components/shared/search-input.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { Input } from "../ui";
 import Link from "next/link";
 import { useClickAway } from "react-use";
+import { Ingredient } from "@prisma/client";
 
 interface Props {
   className?: string;
@@ -11,12 +12,19 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({ className }) => {
   const [searchValue, setSearchValue] = React.useState("");
   const [focused, setFocused] = React.useState(false);
+  const [resIngredients, setResIngredients] = React.useState<Ingredient[]>([]);
   const ref = React.useRef(null);
   useClickAway(ref, () => {
     setFocused(false);
   });
-  const changeSearchHandler = (e: string) => {
-    setSearchValue(e);
+  const changeSearchHandler = async (event: string) => {
+    setSearchValue(event);
+    const res = await fetch("http://localhost:3000/api/ingredients");
+    const data: Ingredient[] = await res.json();
+    const data_filtered = data.filter((e) =>
+      e.name.toLowerCase().includes(event.toLowerCase())
+    );
+    setResIngredients(data_filtered);
   };
   return (
     <div className={cn(className)} ref={ref}>
@@ -27,24 +35,30 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
         onClick={() => setFocused(true)}
         onChange={(e) => changeSearchHandler(e.target.value)}
       />
-      <div
-        className={cn(
-          "absolute bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
-          focused && "visible opacity-100 top-[60]",
-          className
-        )}
-      >
-        <Link
-          className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-          href={`/product/`}
+      {resIngredients.length > 0 && (
+        <div
+          className={cn(
+            "absolute bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+            focused && "visible opacity-100 top-[60]",
+            className
+          )}
         >
-          <img
-            className="rounded-sm h-8 w-8"
-            src="https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png"
-          />
-          <span>12</span>
-        </Link>
-      </div>
+          {resIngredients.map((product) => (
+            <Link
+              key={product.id}
+              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+              href={`/product/${product.id}`}
+            >
+              <img
+                className="rounded-sm h-8 w-8"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+              <span>{product.name}</span>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
