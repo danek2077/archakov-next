57f5a4e10082b52e38334c7173377987423b7263
danek2077 <pprezident337@gmail.com>
2024-12-16 17:00:41 +0100

    dev

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../prisma/db";
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get("query") || "";
+  const data = prisma.ingredient.findMany({
+    where: { name: { contains: query, mode: "insensitive" } },
+    take:5
+  });
+  return NextResponse.json(data);
+}
